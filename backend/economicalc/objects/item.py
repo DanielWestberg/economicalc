0a91ca6f67c0505fa82aada86713c313d80047ef
Axel <acke756@gmail.com>
2022-11-17 17:12:03 +0100

    Store price as int rather than float

--- backend/economicalc/objects/item.py
+++ backend/economicalc/objects/item.py
@@ -1,5 +1,5 @@
 class Item:
-    def __init__(self, item_name: str, price: float) -> None:
+    def __init__(self, item_name: str, price: int) -> None:
         self.item_name = item_name
         self.price = price
 
