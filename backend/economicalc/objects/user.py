4ba3eedac5064f8cbedc3d7b451e628e39419c8b
Axel <acke756@gmail.com>
2022-12-05 14:53:25 +0100

    Added category list to backend user model

--- backend/economicalc/objects/user.py
+++ backend/economicalc/objects/user.py
@@ -1,24 +1,28 @@
 from typing import Any, Dict, List
 
 from .receipt import Receipt
+from .category import Category
 
 
 class User:
-    def __init__(self, bankId, receipts: List[Receipt]) -> None:
+    def __init__(self, bankId, receipts: List[Receipt], categories: List[Category]) -> None:
         self.bankId = bankId
         self.receipts = receipts
+        self.categories = categories
 
     def __eq__(self, other: Any) -> bool:
         return (
             type(self) == type(other) and
             self.bankId == other.bankId and
-            all([expected == actual for (expected, actual) in zip(self.receipts, other.receipts)])
+            all([expected == actual for (expected, actual) in zip(self.receipts, other.receipts)]) and
+            all([expected == actual for (expected, actual) in zip(self.categories, other.categories)])
         )
 
     def to_dict(self, json_serializable=False) -> None:
         return {
             "bankId": self.bankId,
             "receipts": [receipt.to_dict(json_serializable) for receipt in self.receipts],
+            "categories": [category.to_dict(json_serialiazble) for category in self.categories],
         }
 
     @staticmethod
