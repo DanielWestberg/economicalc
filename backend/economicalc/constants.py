da8fcccc7e5b61e9ba323e98e902933c2beb2102
Axel <acke756@gmail.com>
2022-12-05 16:07:47 +0100

    Category POST requests can no longer include IDs that already exist

--- backend/economicalc/constants.py
+++ backend/economicalc/constants.py
@@ -4,5 +4,6 @@ no_content = "204 NO CONTENT"
 
 bad_request = "400 BAD REQUEST"
 not_found = "404 NOT FOUND"
+conflict = "409 CONFLICT"
 unsupported_media_type = "415 UNSUPPORTED MEDIA TYPE"
 unprocessable_entity = "422 UNPROCESSABLE ENTITY"
