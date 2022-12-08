bb037fb2146b43d9a99d6b851428e9b3df837398
Axel <acke756@gmail.com>
2022-12-08 16:35:26 +0100

    Added backend function for posting multiple receipts

--- backend/economicalc/app.py
+++ backend/economicalc/app.py
@@ -21,8 +21,10 @@ def create_app(config):
     def user_receipts(bankId):
         if request.method == "GET":
             return get_receipts(bankId, request)
+        if type(request.json) == list:
+            return post_many_receipts(bankId, request)
         
-        return post_receipts(bankId, request)
+        return post_one_receipt(bankId, request)
 
 
     @app.route('/initiate_bank_session/')
@@ -116,10 +118,11 @@ def create_app(config):
         return receipt
 
 
-    def post_receipts(bankId, request):
+    def post_one_receipt(bankId, request):
         required_type = "application/json"
         if request.content_type[:len(required_type)] != "application/json":
             return make_response(f"Expected content type application/json, not {request.content_type}", unsupported_media_type)
+
         receipt_dict = request.json
         receipt_dict.pop("_id", None)
         receipt_dict.pop("imageId", None)
@@ -136,6 +139,39 @@ def create_app(config):
         db.users.update_one({"_id": user["_id"]}, update_action)
 
         return make_response(jsonify(data=receipt.to_dict(True)), created)
+
+
+    def post_many_receipts(bankId, request):
+        required_type = "application/json"
+        if request.content_type[:len(required_type)] != "application/json":
+            return make_response(f"Expected content type application/json, not {request.content_type}", unsupported_media_type)
+
+        receipt_dicts = request.json
+
+        receipts = []
+        for receipt_dict in receipt_dicts:
+            receipt_dict.pop("_id", None)
+            receipt_dict.pop("imageId", None)
+
+            receipt = parse_receipt_or_make_response(receipt_dict)
+            if type(receipt) != Receipt:
+                return receipt
+
+            if len(receipt.items) == 0:
+                return make_response("Field \"items\" may not be an empty list", unprocessable_entity)
+
+            receipts.append(receipt)
+
+        receipt_dicts = [r.to_dict() for r in receipts]
+        user = db.users.find_one_or_404({"bankId": bankId})
+        update_action = {"$push": {"receipts": {"$each": receipt_dicts}}}
+        db.users.update_one({"_id": user["_id"]}, update_action)
+
+        for receipt_dict in receipt_dicts:
+            Receipt.make_json_serializable(receipt_dict)
+
+        return make_response(jsonify(data=receipt_dicts), created)
+
 
     @app.route("/users/<bankId>/receipts/<ObjectId:receiptId>", methods=["PUT", "DELETE"])
     def user_receipt_by_id(bankId, receiptId):
