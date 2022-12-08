ec5e82a034430ce98ea0c283745069ab1b523e46
kakann <nitram.eriksson@hotmail.com>
2022-12-08 14:43:05 +0100

    changed port from 5000 to 443(https port) for backend

--- backend/economicalc/app.py
+++ backend/economicalc/app.py
@@ -210,6 +210,6 @@ def create_db(app):
 
 if __name__ == "__main__":
     ENVIRONMENT_DEBUG = os.environ.get("APP_DEBUG", True)
-    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 5000)
+    ENVIRONMENT_PORT = os.environ.get("APP_PORT", 443)
     app = create_app(FlaskConfig())
     app.run(host='0.0.0.0', port=ENVIRONMENT_PORT, debug=ENVIRONMENT_DEBUG)
