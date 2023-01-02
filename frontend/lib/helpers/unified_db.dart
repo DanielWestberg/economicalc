f6eb1a0710200cd731d3b78b3eb636bc2f133d2b
Axel <acke756@gmail.com>
2023-01-02 14:17:14 +0100

    Added initialization to Unified DB

--- frontend/lib/helpers/unified_db.dart
+++ frontend/lib/helpers/unified_db.dart
@@ -7,7 +7,18 @@ import 'package:economicalc_client/models/transaction.dart';
 import 'package:economicalc_client/models/bank_transaction.dart';
 import 'package:economicalc_client/services/api_calls.dart';
 
+import 'package:sqflite/sqflite.dart' show DatabaseFactory;
+
 // Acts as common interface for local db and backend db.
 class UnifiedDb extends SQFLite {
   final ApiCaller _apiCaller = ApiCaller();
+
+  UnifiedDb({DatabaseFactory? dbFactory, Future<String> Function()? path}) :
+    super(dbFactory: dbFactory, path: path);
+
+  static UnifiedDb? _instance;
+  static UnifiedDb get instance {
+    _instance ??= UnifiedDb();
+    return _instance!;
+  }
 }
